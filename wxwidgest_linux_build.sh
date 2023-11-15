8b96f24a1e19a08e161903382df4a123a737e72f
Talipov S.N <proffix4@gmail.com>
2023-11-15 16:33:38 +0600

    update

--- wxwidgest_linux_build.sh
+++ wxwidgest_linux_build.sh
@@ -3,7 +3,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install build-essential gdb wget mc
+sudo apt install build-essential libgtk-3-dev gdb wget mc -y
 
 wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.4/wxWidgets-3.2.4.tar.bz2
 tar xf wxWidgets-3.2.4.tar.bz2
@@ -12,7 +12,7 @@ cd wxWidgets-3.2.4
 
 mkdir buildgtk
 cd buildgtk
-../configure --with-gtk --disable-shared  --enable-unicode
+../configure --disable-shared  --enable-unicode
 make -j$(nproc --all)
 sudo make install
 sudo ldconfig
