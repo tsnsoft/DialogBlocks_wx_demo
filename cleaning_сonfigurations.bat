4bfe9d4c79af5f270c25ffc094fea167973f5581
Talipov S.N. <proffix4@gmail.com>
2025-02-21 14:20:37 +0500

    rev. 20250221142037

--- cleaning_сonfigurations.bat
+++ cleaning_сonfigurations.bat
@@ -2,4 +2,4 @@
 del /Q *.bak *.sln *.vcxproj *.vcxproj.user >nul 2>&1
 del /Q makefile.gcc >nul 2>&1
 del /Q Makefile >nul 2>&1
-echo Cleaning of project configurations is completed
+echo Cleaning of project configurations is completed.
