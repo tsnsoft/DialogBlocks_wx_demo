a9bbd2f86e4bb0bba3ee49b2ec1233f278880ee7
Talipov S.N. <proffix4@gmail.com>
2025-02-21 14:10:12 +0500

    rev. 20250221141012

--- clean_msvc_project.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-del /Q *.sln *.vcxproj *.vcxproj.user
-echo MSVS project files have been deleted.
---
+++ cleaning_сonfigurations.bat
@@ -0,0 +1,5 @@
+@echo off
+del /Q *.bak *.sln *.vcxproj *.vcxproj.user >nul 2>&1
+del /Q makefile.gcc >nul 2>&1
+del /Q Makefile >nul 2>&1
+echo Cleaning of project configurations is completed
